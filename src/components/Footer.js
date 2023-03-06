8ec4d3ecf7e2fe943d0ab2872130ac46e8212477
Jayaditya-Contributor <OpenSourceAditya@gmail.com>
2023-03-06 12:03:14 +0530

    animated button and fixed eslint errors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,16 +8,16 @@ function Footer() {
     return (
         <div className="footer">
             <div className="socialMedia">
-                <a href="https://www.linkedin.com/in/bonniechin" target="_blank">
+                <a href="https://www.linkedin.com/in/bonniechin" target="_blank" rel="noreferrer">
                     <BsLinkedin />
                 </a>
-                <a href="https://www.instagram.com/bonnie.chin/" target="_blank">
+                <a href="https://www.instagram.com/bonnie.chin/" target="_blank" rel="noreferrer">
                     <BsInstagram />
                 </a>
-                <a href="https://github.com/bcchin" target="_blank">
+                <a href="https://github.com/bcchin" target="_blank" rel="noreferrer">
                     <BsGithub />
                 </a>
-                <a href="https://www.flickr.com/photos/148300792@N03/" target="_blank">
+                <a href="https://www.flickr.com/photos/148300792@N03/" target="_blank" rel="noreferrer">
                     <FaFlickr />
                 </a>
             </div>
